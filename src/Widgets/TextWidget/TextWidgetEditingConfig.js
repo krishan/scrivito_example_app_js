21c6bc711677d5dbaa4e1ea5d7016acacacaf4aa
Kristian Hanekamp <kris.hanekamp@gmail.com>
2020-03-04 11:20:38 +0100

    Prototype for a "text change handler"

--- src/Widgets/TextWidget/TextWidgetEditingConfig.js
+++ src/Widgets/TextWidget/TextWidgetEditingConfig.js
@@ -23,4 +23,42 @@ Scrivito.provideEditingConfig("TextWidget", {
   initialContent: {
     alignment: "left",
   },
+  validations: [
+    [
+      "text",
+      (_ignore, { widget }) => {
+        // enqueue the "change handler" in a new browser event
+        setTimeout(() => onNewText(widget), 0);
+
+        // we don't actually want to validate anything
+        return false;
+      },
+    ],
+  ],
 });
+
+// this handler will be invoked when text is changed,
+// but it is also sometimes invoked when text is unchanged, e.g. intially.
+//
+// beware: the handler will be invoked often, so if you need to perform
+// a compute-intensive task, use throtteling.
+function onNewText(widget) {
+  // bail out if the content cannot be changed
+  if (!Scrivito.canWrite()) return;
+
+  // this is just a simple example of what one can do with this:
+  // if the editor types an umlaut, replace it with a non-umlaut character.
+  // make sure that any content manipulation done here is idempotent,
+  // to avoid creating endless loops.
+  widget.update({ text: replaceUmlauts(widget.get("text")) });
+}
+
+function replaceUmlauts(text) {
+  const replacements = {
+    ä: "a",
+    ö: "o",
+    ü: "u",
+  };
+
+  return text.replace(/[äöü]/g, umlaut => replacements[umlaut]);
+}
